5d13e5e41944d6bb64f9d6914fe89201004b5592
jeffreyxazer <jeffreyxazer@gmail.com>
2025-03-04 20:24:11 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
 }
+
+function addingEventListener() {
+  
+  const button = document.getElementById('button');
+
+  
+  button.addEventListener('click', function() {
+    console.log('Button clicked!');
+  });
+}
+
+addingEventListener();
